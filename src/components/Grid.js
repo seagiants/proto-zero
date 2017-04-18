df32e421fa4ea88eeb17a28c3edeedb248c9fa77
romaintailhurat <romain.tailhurat@gmail.com>
2017-04-18 22:09:13 +0200

    Adding some keys to components

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,10 +1,11 @@
 import React from "react";
 import range from "lodash.range";
+import uniqueId from 'lodash.uniqueid';
 import Row from "./Row";
 
 const Grid = ({ x, y }) => (
   <div>
-    {range(x).map((e, i) => <Row size={y} />)}
+    {range(x).map((e, i) => <Row key={uniqueId()} size={y} />)}
   </div>
 );
 
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Cell from "./Cell";
 import range from "lodash.range";
+import uniqueId from 'lodash.uniqueid';
 
 const Row = ({ size }) => (
   <div>
-    {range(size).map(() => <Cell />)}
+    {range(size).map(() => <Cell key={uniqueId()}/>)}
   </div>
 );
 
