d2317d05ced7546c91f3542a02809e12c337528f
romaintailhurat <romain.tailhurat@gmail.com>
2017-05-11 23:34:46 +0200

    Adding some tests to cell generation functions

--- src/engine/map.js
+++ src/engine/map.js
@@ -1,16 +1,16 @@
-import random from 'lodash.random';
+import random from "lodash.random";
 
 const baseTypes = [
-  {name: 'river', color: 'blue', hidden : true},
-  {name: 'mountain', color: 'grey', hidden : true},
-  {name: 'grass', color: 'green', hidden : true}
+  { name: "river", color: "blue", hidden: true },
+  { name: "mountain", color: "grey", hidden: true },
+  { name: "grass", color: "green", hidden: true }
 ];
 
-function generateType(baseType, x, y) {
-  return {...baseType, x: x, y: y};
+export function generateType(baseType, x, y) {
+  return { ...baseType, x: x, y: y };
 }
 
-function randType(i,j){
+export function randType(i, j) {
   let base = baseTypes[random(baseTypes.length - 1)];
   let gen = generateType(base, i, j);
   return gen;
@@ -19,10 +19,10 @@ function randType(i,j){
 /* Generate an x by y matrix, each cell being of a particular type */
 export const generateMap = (x, y) => {
   let map = [];
-  for (let i = 0; i < x;  i++) {
+  for (let i = 0; i < x; i++) {
     map.push([]);
-    for (let j = 0 ; j < y ; j++) {
-      map[i].push(randType(i,j));
+    for (let j = 0; j < y; j++) {
+      map[i].push(randType(i, j));
     }
   }
   return map;
