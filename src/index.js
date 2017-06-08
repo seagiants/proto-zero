b9eccac868f4dab53f338eed0edc03d0360cad19
romaintailhurat <romain.tailhurat@gmail.com>
2017-06-08 08:39:12 +0200

    New game request to the server, and a bit of cleaning

--- src/index.js
+++ src/index.js
@@ -8,7 +8,20 @@ import appState from "./reducers";
 
 let store = createStore(
   appState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+fetch("http://localhost:9000/newgame")
+  .then(response => {
+    console.log("Response from the server");
+    return response.json();
+  })
+  .then(data => {
+    console.log("Game data is", data);
+  })
+  .catch(error => {
+    console.error("Error when trying to reach the server", error);
+  });
 
 ReactDOM.render(
   <Provider store={store}>
