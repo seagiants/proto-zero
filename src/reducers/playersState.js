e4db1a6d29af1962368a2a086cd52195e87a1db5
romaintailhurat <romain.tailhurat@gmail.com>
2017-09-23 15:32:54 +0200

    A pass of prettier on playerState

--- src/reducers/playersState.js
+++ src/reducers/playersState.js
@@ -1,103 +1,156 @@
 import { generateBoard, drawCards } from "../engine";
-import { REFRESH_POWER_BOARD, DRAW, SELECTED_CARD, PRODUCE, TAP_POWER_CASE, POWER_SELECTION } from "../actions"
+import {
+  REFRESH_POWER_BOARD,
+  DRAW,
+  SELECTED_CARD,
+  PRODUCE,
+  TAP_POWER_CASE,
+  POWER_SELECTION
+} from "../actions";
 const initialState = {
   playerOne: {
     name: "Bibi",
-    playerBoard : generateBoard()
-
+    playerBoard: generateBoard()
   },
   playerTwo: {
     name: "BustedKeaton",
-    playerBoard : generateBoard()
+    playerBoard: generateBoard()
   }
 };
 
-const addingCardToPowerBoard = (powerBoard,card) => {
-  return powerBoard.map( (powerCase) => {
-        if(powerCase.categoryName!==card.category.name) {
-            return {...powerCase};
-        }
-        return {
-            ...powerCase, card : card
-        };
-    });
-};
-
-const refreshingPowerCases = (powerBoard) => {
-  return powerBoard.map((powerCase)=>{
-    return{...powerCase, isTapped: false, isSelected: false};
+const addingCardToPowerBoard = (powerBoard, card) => {
+  return powerBoard.map(powerCase => {
+    if (powerCase.categoryName !== card.category.name) {
+      return { ...powerCase };
+    }
+    return {
+      ...powerCase,
+      card: card
+    };
   });
 };
 
-const selectingPowerCaseFromPowerBoard = (powerBoard, categoryName,newStatus=true) => {
-  return powerBoard.map((powerCase) => {
-    if(powerCase.categoryName === categoryName){
-      return {...powerCase, isSelected:newStatus};
-    }else{ return {...powerCase}
-    }
-  })
+const refreshingPowerCases = powerBoard => {
+  return powerBoard.map(powerCase => {
+    return { ...powerCase, isTapped: false, isSelected: false };
+  });
 };
 
-
-const togglingPowerCaseFromPowerBoard = (powerBoard,categoryName,newStatus=true) => {
-  return powerBoard.map((powerCase) => {
-    if(powerCase.categoryName === categoryName){
-      return {...powerCase, isTapped:newStatus, isSelected: false};
-    }else{ return {...powerCase, isSelected: false}
+const selectingPowerCaseFromPowerBoard = (
+  powerBoard,
+  categoryName,
+  newStatus = true
+) => {
+  return powerBoard.map(powerCase => {
+    if (powerCase.categoryName === categoryName) {
+      return { ...powerCase, isSelected: newStatus };
+    } else {
+      return { ...powerCase };
     }
-  })
+  });
 };
 
-
+const togglingPowerCaseFromPowerBoard = (
+  powerBoard,
+  categoryName,
+  newStatus = true
+) => {
+  return powerBoard.map(powerCase => {
+    if (powerCase.categoryName === categoryName) {
+      return { ...powerCase, isTapped: newStatus, isSelected: false };
+    } else {
+      return { ...powerCase, isSelected: false };
+    }
+  });
+};
 
 export const playersState = (state = initialState, action) => {
-
   switch (action.type) {
-    case DRAW :
-      return { ...state, [action.player] : {
-          ...state[action.player], playerBoard : {
-            ...state[action.player].playerBoard, drawBoard : drawCards(3)
+    case DRAW:
+      return {
+        ...state,
+        [action.player]: {
+          ...state[action.player],
+          playerBoard: {
+            ...state[action.player].playerBoard,
+            drawBoard: drawCards(3)
+          }
         }
-      }
-    };
-    case SELECTED_CARD :
-      return { ...state, [action.player] : {
-                ...state[action.player], playerBoard : {
-                  ...state[action.player].playerBoard, drawBoard : null, powerBoard : addingCardToPowerBoard(state[action.player].playerBoard.powerBoard,action.card)
-                }
-            }
-          };
-    case PRODUCE :
-      let newResourceCounter = state[action.player].playerBoard.resourceCounter + action.powerProps.quantity;
-      return { ...state, [action.player] : {
-          ...state[action.player], playerBoard : {
-            ...state[action.player].playerBoard, resourceCounter : newResourceCounter
+      };
+    case SELECTED_CARD:
+      return {
+        ...state,
+        [action.player]: {
+          ...state[action.player],
+          playerBoard: {
+            ...state[action.player].playerBoard,
+            drawBoard: null,
+            powerBoard: addingCardToPowerBoard(
+              state[action.player].playerBoard.powerBoard,
+              action.card
+            )
+          }
+        }
+      };
+    case PRODUCE:
+      let newResourceCounter =
+        state[action.player].playerBoard.resourceCounter +
+        action.powerProps.quantity;
+      return {
+        ...state,
+        [action.player]: {
+          ...state[action.player],
+          playerBoard: {
+            ...state[action.player].playerBoard,
+            resourceCounter: newResourceCounter
           }
         }
       };
 
-    case TAP_POWER_CASE :
-      return { ...state, [action.player] : {
-                ...state[action.player], playerBoard : {
-                  ...state[action.player].playerBoard, powerBoard : togglingPowerCaseFromPowerBoard(state[action.player].playerBoard.powerBoard,action.categoryName,true)
-                }
-              }
-            };
-    case REFRESH_POWER_BOARD :
-      return { ...state, [action.player] : {
-                ...state[action.player], playerBoard : {
-                  ...state[action.player].playerBoard, powerBoard : refreshingPowerCases(state[action.player].playerBoard.powerBoard)
-                }
-              }
-            };
-    case POWER_SELECTION :
-    return { ...state, [action.player] : {
-              ...state[action.player], playerBoard : {
-                ...state[action.player].playerBoard, powerBoard : selectingPowerCaseFromPowerBoard(state[action.player].playerBoard.powerBoard, action.powerCase.categoryName,true)
-              }
-            }
-          };
-  /*  case DISCOVER_CELL :
+    case TAP_POWER_CASE:
+      return {
+        ...state,
+        [action.player]: {
+          ...state[action.player],
+          playerBoard: {
+            ...state[action.player].playerBoard,
+            powerBoard: togglingPowerCaseFromPowerBoard(
+              state[action.player].playerBoard.powerBoard,
+              action.categoryName,
+              true
+            )
+          }
+        }
+      };
+    case REFRESH_POWER_BOARD:
+      return {
+        ...state,
+        [action.player]: {
+          ...state[action.player],
+          playerBoard: {
+            ...state[action.player].playerBoard,
+            powerBoard: refreshingPowerCases(
+              state[action.player].playerBoard.powerBoard
+            )
+          }
+        }
+      };
+    case POWER_SELECTION:
+      return {
+        ...state,
+        [action.player]: {
+          ...state[action.player],
+          playerBoard: {
+            ...state[action.player].playerBoard,
+            powerBoard: selectingPowerCaseFromPowerBoard(
+              state[action.player].playerBoard.powerBoard,
+              action.powerCase.categoryName,
+              true
+            )
+          }
+        }
+      };
+    /*  case DISCOVER_CELL :
 
         return {..., [action.player] : {
             ...state[action.player], playerBoard: {
@@ -106,7 +159,7 @@ export const playersState = (state = initialState, action) => {
         }
       }*/
     default:
-      console.log("Action not supported : "+action.type);
+      console.log("Action not supported : " + action.type);
       return state;
   }
 };
