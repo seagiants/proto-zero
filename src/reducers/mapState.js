08799c27354dde8b4f18f56f5ed9466532d0f6d5
romaintailhurat <romain.tailhurat@gmail.com>
2017-09-27 22:32:03 +0200

    Cleaning mapState off some unused functions

--- src/reducers/mapState.js
+++ src/reducers/mapState.js
@@ -1,17 +1,9 @@
 import {
-  GENERATE_MAP,
   DISCOVER_CELL,
   POWER_SELECTION,
   STORE_MAP
 } from "../actions";
-import { generateMap } from "../engine";
 import { getActivePower } from "../engine/powerLogic";
-import { mapDimensions } from "../constants";
-
-const initialState = {
-  gameMap: generateMap(mapDimensions.width, mapDimensions.height),
-  activePlayer: "playerOne"
-};
 
 const showCell = (gameMap, x, y) => {
   return gameMap.map((row, index) => {
@@ -34,8 +26,6 @@ const showCell = (gameMap, x, y) => {
 
 export const mapState = (state = {}, action) => {
   switch (action.type) {
-    //case GENERATE_MAP:
-    //  return { ...state, gameMap: generateMap(action.x, action.y) };
     case STORE_MAP:
       return { ...state, gameMap: action.map };
     case DISCOVER_CELL:
