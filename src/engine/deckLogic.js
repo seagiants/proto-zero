225f68e861e5fbdf41adfbe414eaae6438718eca
romaintailhurat <romain.tailhurat@gmail.com>
2018-02-28 20:10:43 +0100

    Fixing names in libraries, and a bit of prettier

--- src/engine/deckLogic.js
+++ src/engine/deckLogic.js
@@ -1,42 +1,50 @@
-import { cardLibrairy, upgradedCardLibrairy } from "../libraries/cardLib.js";
+import { cardLibrary, upgradedCardLibrary } from "../libraries/cardLib.js";
 import random from "lodash.random";
 
-export const shuffleCards = (cards) => {
+export const shuffleCards = cards => {
   console.log(cards);
-  return cards.slice(0).sort((a,b)=>(0.5-random(1)))
-}
-export const drawCards = (size,deckState) => {
-  let newDeckState = deckState.draw.length < size?reShuffleDiscard(deckState):deckState
+  return cards.slice(0).sort((a, b) => 0.5 - random(1));
+};
+export const drawCards = (size, deckState) => {
+  let newDeckState =
+    deckState.draw.length < size ? reShuffleDiscard(deckState) : deckState;
   let draw = newDeckState.draw.slice(size);
-  let toPick = newDeckState.draw.slice(0,size);
+  let toPick = newDeckState.draw.slice(0, size);
   let discard = newDeckState.discard.concat(deckState.toPick);
   return {
-    draw:draw,
-    toPick:toPick,
-    discard:discard
-    }
+    draw: draw,
+    toPick: toPick,
+    discard: discard
   };
+};
 
-  export const reShuffleDiscard = (deckState) => ({
-    draw:deckState.draw.concat(deckState.discard.sort((a,b)=>(0.5-random(1)))),
-    toPick:deckState.toPick,
-    discard:[]
-  });
+export const reShuffleDiscard = deckState => ({
+  draw: deckState.draw.concat(
+    deckState.discard.sort((a, b) => 0.5 - random(1))
+  ),
+  toPick: deckState.toPick,
+  discard: []
+});
 
-  export const generateCard = (cardTemplate) => {
-    return {...cardTemplate};
-  };
+export const generateCard = cardTemplate => {
+  return { ...cardTemplate };
+};
 
-  export const randCard = () => cardLibrairy[random(cardLibrairy.length - 1)];
+export const randCard = () => cardLibrary[random(cardLibrary.length - 1)];
 
-  export const getCard = (name) => cardLibrairy.filter((element) => (element.powerName === name) )[0];
+export const getCard = name =>
+  cardLibrary.filter(element => element.powerName === name)[0];
 
-  export const getUpgradedCard = (name) => upgradedCardLibrairy.filter((element) => (element.powerName === name) )[0];
+export const getUpgradedCard = name =>
+  upgradedCardLibrary.filter(element => element.powerName === name)[0];
 
-  export const addUpgradedCardToDraw = (draw,upgradedCard) => shuffleCards(draw.concat([getUpgradedCard(upgradedCard)]));
+export const addUpgradedCardToDraw = (draw, upgradedCard) =>
+  shuffleCards(draw.concat([getUpgradedCard(upgradedCard)]));
 
-  export const generateDeck = () => ({
-    draw: cardLibrairy.map(card => generateCard(card)).sort((a,b)=>(0.5-random(1))),
-    toPick: [],
-    discard: []
-  });
+export const generateDeck = () => ({
+  draw: cardLibrary
+    .map(card => generateCard(card))
+    .sort((a, b) => 0.5 - random(1)),
+  toPick: [],
+  discard: []
+});
--- src/libraries/cardLib.js
+++ src/libraries/cardLib.js
@@ -1,142 +1,148 @@
 import * as actions from "../actions";
-import { getCategory } from "./powerLib.js"
-import { hasSpecificBuilding, hasNeighboursSpecificBuilding } from "../engine/mapLogic.js"
+import { getCategory } from "./powerLib.js";
+import {
+  hasSpecificBuilding,
+  hasNeighboursSpecificBuilding
+} from "../engine/mapLogic.js";
 
 //FIXME need entity mechanism. Card = power + cost, same entity or not ?
-export const cardLibrairy = [
+export const cardLibrary = [
   {
     powerName: "Vision",
     category: getCategory("EXPLORATION"),
     powerAction: actions.discoverCell,
     symbol: "vision",
-    isTargetRequired : true,
+    isTargetRequired: true,
     cost: 2,
-    powerProps : {
-      persistent: 0 ,
+    powerProps: {
+      persistent: 0,
       radius: 1
-      }
-    },
+    }
+  },
   {
     powerName: "Radar",
     category: getCategory("EXPLORATION"),
     powerAction: actions.discoverCell,
     symbol: "vision",
-    isTargetRequired : true,
+    isTargetRequired: true,
     cost: 7,
-    powerProps : {
+    powerProps: {
       persistent: 0
-      }
+    }
   },
   {
     powerName: "Factory",
     category: getCategory("ECONOMY"),
     powerAction: actions.build,
     symbol: "build",
-    isTargetRequired : true,
+    isTargetRequired: true,
     cost: 2,
-    powerProps : {
-      persistent: 0 ,
+    powerProps: {
+      persistent: 0,
       build: "Factory"
-      }
+    }
   },
   {
     powerName: "Nexus",
     category: getCategory("ECONOMY"),
     powerAction: actions.build,
     symbol: "build",
-    isTargetRequired : true,
+    isTargetRequired: true,
     cost: 4,
-    powerProps : {
-      persistent: 0 ,
+    powerProps: {
+      persistent: 0,
       build: "Nexus"
-      }
+    }
   },
   {
     powerName: "Missile",
     category: getCategory("MILITARY"),
     powerAction: actions.fireMissile,
     symbol: "missile",
-    checkConditions: (x,y,state) => hasSpecificBuilding(x,y,state.mapState.gameMap,true),
-    isTargetRequired : true,
+    checkConditions: (x, y, state) =>
+      hasSpecificBuilding(x, y, state.mapState.gameMap, true),
+    isTargetRequired: true,
     cost: 2,
-    powerProps : {
+    powerProps: {
       persistent: 0,
       evolution: "Missile 2"
-      }
+    }
   },
   {
     powerName: "Rocket",
     category: getCategory("MILITARY"),
     powerAction: actions.fireRocket,
     symbol: "missile",
-    checkConditions : (x,y,state) => hasNeighboursSpecificBuilding(x,y,state.mapState.gameMap,"Nexus"),
-    isTargetRequired : true,
+    checkConditions: (x, y, state) =>
+      hasNeighboursSpecificBuilding(x, y, state.mapState.gameMap, "Nexus"),
+    isTargetRequired: true,
     cost: 2,
-    powerProps : {
+    powerProps: {
       persistent: Infinity
-      }
+    }
   },
   {
     powerName: "Increased Radar",
     category: getCategory("TECHNOLOGY"),
     powerAction: actions.enhancement,
     symbol: "enhancement",
-    isTargetRequired : false,
+    isTargetRequired: false,
     cost: 0,
-    powerProps : {
-      persistent: 0 ,
-      enhancements : {
-          "TECHNOLOGY" : {
-            cost: 1
-          },
-          "EXPLORATION" : {
-            radius: 1,
-            cost: 1
-          }
+    powerProps: {
+      persistent: 0,
+      enhancements: {
+        TECHNOLOGY: {
+          cost: 1
+        },
+        EXPLORATION: {
+          radius: 1,
+          cost: 1
         }
       }
+    }
   },
   {
     powerName: "Enhanced Research",
     category: getCategory("TECHNOLOGY"),
     powerAction: actions.enhancement,
     symbol: "enhancement",
-    isTargetRequired : false,
+    isTargetRequired: false,
     cost: 0,
-    powerProps : {
-      persistent: 0 ,
-      enhancements : {
-          "TECHNOLOGY" : {
-            cost: -1
-          },
+    powerProps: {
+      persistent: 0,
+      enhancements: {
+        TECHNOLOGY: {
+          cost: -1
         }
       }
+    }
   },
   {
     powerName: "Shield",
     category: getCategory("MILITARY"),
     powerAction: actions.upgradeBuilding,
     symbol: "build",
-    isTargetRequired : true,
+    isTargetRequired: true,
     cost: 2,
-    powerProps : {
+    powerProps: {
       persistent: 0,
       build: "Shield"
-      }
+    }
   }
 ];
 
-export const upgradedCardLibrairy = [
+export const upgradedCardLibrary = [
   {
     powerName: "Missile 2",
     category: getCategory("MILITARY"),
     powerAction: actions.fireMissile,
     symbol: "missile",
-    checkConditions: (x,y,state) => hasSpecificBuilding(x,y,state.mapState.gameMap,true),
-    isTargetRequired : true,
+    checkConditions: (x, y, state) =>
+      hasSpecificBuilding(x, y, state.mapState.gameMap, true),
+    isTargetRequired: true,
     cost: 2,
-    powerProps : {
+    powerProps: {
       persistent: 1
-      }
+    }
   }
-]
+];
